d0d9efda1ad0ab350329dd97452cbe0501394109
noah2296 <noah2296@gmail.com>
2017-11-01 15:18:07 -0500

    Added PermissionExplorer class to open the app permissions screen and will enable all permissions for the app.

---
+++ app/src/main/java/andrews/cloakanddagger/PermissionExplorer.java
@@ -0,0 +1,18 @@
+package andrews.cloakanddagger;
+
+import android.content.Context;
+
+/**
+ * Created by slothlord on 11/1/17.
+ */
+
+public class PermissionExplorer {
+
+    private Context parent;
+
+    public PermissionExplorer(Context ctx) {
+        parent = ctx;
+    }
+
+
+}
